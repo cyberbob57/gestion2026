6b0859df0036fa8675d5db6e7e8b1b960fc39ba5
CYBERBOB <cyberbob@Mac-mini-de-CYBERBOB.local>
2026-05-19 12:11:06 +0200

    Versionnement assets (app.js/css ?v=37) pour casser le cache definitivement

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gestion2026-v37';
+const CACHE = 'gestion2026-v38';
 const ASSETS = ['./index.html', './app.css', './app.js', './icon.svg'];
 
 self.addEventListener('install', e => {
